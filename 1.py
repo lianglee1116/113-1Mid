c08cd12bf28b5f9bfdc65e3175ade5d233771ee3
lianglee1116  <112111206@mail.aeust.edu.tw>
2024-11-04 15:07:00 +0800

    123

--- 1.py
+++ 1.py
@@ -1,58 +1,3 @@
-# 期中考
->
->學號：112111206
-><br />
->姓名：李儲亮
-><br />
->作業撰寫時間：180 (mins，包含程式撰寫時間)
-><br />
->最後撰寫文件日期：2024/11/04
->
-
-本份文件包含以下主題：(至少需下面兩項，若是有多者可以自行新增)
-- [x] 說明內容
-
-## 說明程式與內容
-
-開始寫說明，該說明需說明想法，
-並於之後再對上述想法的每一部分將程式進一步進行展現，
-若需引用程式區則使用下面方法，
-若為.cs檔內程式除了於敘述中需註明檔案名稱外，
-還需使用語法` ```語言種類 程式碼 ``` `，其中語言種類若是要用python則使用py，java則使用java，C/C++則使用cpp，
-下段程式碼為語言種類選擇csharp使用後結果：
-
-```csharp
-public void mt_getResult(){
-    ...
-}
-```
-
-若要於內文中標示部分網頁檔，則使用以下標籤` ```html 程式碼 ``` `，
-下段程式碼則為使用後結果：
-
-```html
-<%@ Page Language="C#" AutoEventWireup="true" ...>
-
-<!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-<meta http-equiv="Content-Type" ...>
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
-</body>
-</html>
-```
-更多markdown方法可參閱[https://ithelp.ithome.com.tw/articles/10203758](https://ithelp.ithome.com.tw/articles/10203758)
-
-a. 小題
-
-Ans
 
 # 創建一個 10x10 的二維陣列，所有元素初始化為 0
 map = [[0 for _ in range(10)] for _ in range(10)]
@@ -60,22 +5,12 @@ map = [[0 for _ in range(10)] for _ in range(10)]
 # 輸出結果以確認
 for row in map:
     print(row)
-
-
-b. 小題
-
-Ans
-
-# 創建一個大小為 10 的一維陣列，所有元素初始化為 0
+    
+    # 創建一個大小為 10 的一維陣列，所有元素初始化為 0
 rowMap = [0 for _ in range(10)]
 
 # 輸出結果以確認
 print(rowMap)
-
-
-c. 小題
-
-Ans
 
 # 創建一個大小為 10 的一維陣列
 rowMap = [0] * 10
@@ -85,11 +20,6 @@ rowMap = [0, 7, 13, 28, 44, 62, 74, 75, 87, 90]
 
 # 輸出結果以確認
 print(rowMap)
-
-
-d. 小題
-
-Ans
 
 # 創建一個 10x10 的二維陣列，所有元素初始化為 0
 map = [[0 for _ in range(10)] for _ in range(10)]
@@ -107,11 +37,6 @@ for value in rowMap:
 for row in map:
     print(' '.join(str(elem) for elem in row))
 
-
-
-e. 小題
-
-Ans
 
 # 創建一個 10x10 的二維陣列，所有元素初始化為 0
 map = [[0 for _ in range(10)] for _ in range(10)]
@@ -153,3 +78,4 @@ for value in rowMap:
 # 輸出結果陣列
 for row in result_map:
     print(' '.join(str(elem) if elem != 0 else ' ' for elem in row))
+
